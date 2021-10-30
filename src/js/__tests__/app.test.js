3fba60324104771b8b283bf30665aea1e66d3b9f
unknown <kgeluza@mail.ru>
2021-10-30 12:56:24 +0400

    commit add

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -5,7 +5,7 @@ test('сортировка игроков по состоянию здоровь
     { name: 'маг', health: 30 },
     { name: 'лучник', health: 48 }];
   const arrSort = sorting(testPlayers);
-  const received = []
-  arrSort.forEach(elem => received.push(elem.health));
+  const received = [];
+  arrSort.forEach((elem) => received.push(elem.health));
   expect(received).toEqual([48, 30, 15]);
 });
